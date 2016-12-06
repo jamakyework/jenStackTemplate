d87290574cbe19d07281c9f1ffa867b764476c65
John Amakye <john.amakye@minneapolismn.gov>
2016-12-06 16:42:48 -0600

    display on DOM working

--- public/scripts/genericScript.js
+++ public/scripts/genericScript.js
@@ -3,6 +3,10 @@ console.log( 'genero sourced' );
 $( document ).ready( function(){
   console.log( 'JQ' );
 
+
+// var getDataArray=[];
+// var postDataArray=[];
+
   // test get function
   var getData = function(){
     console.log( 'in getData' );
@@ -11,12 +15,14 @@ $( document ).ready( function(){
       url: '/testGet',
       success: function( response ){
         console.log( 'back from get call:', response );
+        $( "#outputDiv").append("<p>" + response.field0 + "</p>");
       },
       error: function(){
         console.log( 'error with ajax call...');
       }
-    })
+    });
   }; // end getData
+    // console.log("what is in get data array:", getDataArray);  // check to see if variable was pushed to global var
 
   // test get function
   var postData = function(){
@@ -25,28 +31,40 @@ $( document ).ready( function(){
     var objectToSend={
       tester: 'testy'
     }; // end object to send
+
     $.ajax({
       type: 'POST',
       url: '/testPost',
       data: objectToSend,
       success: function( response ){
         console.log( 'back from post call:', response );
+      displayOnDom
+      $( "#outputDiv").append("<p>" + response.field0 + "</p>");
       },
       error: function(){
         console.log( 'error with ajax call...');
+
       }
-    })
+    });
   }; // end getData
+
+  // console.log("what is in get post array:", postDataArray); // check to see if variable was pushed to global var
 
   /// - buttons to test - ///
   $( '#testGetButton' ).on( 'click', function(){
     console.log( 'in testGetButton on click' );
     getData();
-    $('#outputDiv').append(getData());
+    // displayOnDom(getDataArray);
   }); // end testGetButton
   $( '#testPostButton' ).on( 'click', function(){
     console.log( 'in testPostButton on click' );
     postData();
+    // displayOnDom(postDataArray);
   }); // end testGetButton
+
+  // var displayOnDom = function(testGetFunc){
+  //   $("#outputDiv").empty();
+  //     console.log("In testGet:",testGetFunc);
+  //};
 
 }); //end doc ready
